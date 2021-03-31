34c312f7b161fe9e7e9dabca55bf8d38917abc58
jls06705 <jls06705@sjfc.edu>
2021-03-31 11:24:39 -0400

    switched to tab layout

--- capstone_shiny/CapstoneApp_V2.2.R
+++ capstone_shiny/CapstoneApp_V2.2.R
@@ -7,9 +7,18 @@
 #    http://shiny.rstudio.com/
 #
 
+
+#
+
+
 library(shiny)
 library(shiny)
 library(shinydashboard)
+library(data.table)
+library(rlang)
+library(ggplot2)
+library(tidyverse)
+library(ggQC)
 
 # Define UI for application that draws a histogram
 # ui <- fluidPage(
@@ -34,22 +43,48 @@ library(shinydashboard)
 #     )
 # )
 
-ui <- dashboardPage(
+ui <- fluidPage(
     dashboardHeader(),
+    
+    titlePanel("Tabsets"),
+    
     dashboardSidebar( fileInput("file1", "Choose CSV File",
                                 accept = c(
                                     "text/csv",
                                     "text/comma-separated-values,text/plain",
                                     ".csv")), 
                       # Select variables to display ----
-                      uiOutput("dropdown")
+                      uiOutput("dropdown"),
+                      
+                      numericInput(
+                          "uslId",
+                          "Upper Spec Limit",
+                          value = NULL,
+                          min = NA,
+                          max = NA,
+                          step = NA,
+                          width = NULL
                       ),
-    dashboardBody(box(dataTableOutput("contents"),
-                   plotOutput("hist1"),
-                   plotOutput("hist2")
-                  
-                  
-                  , width = 12, title = "Version 2.2"))
+                      
+                      numericInput(
+                          "lslId",
+                          "Lower Spec Limit",
+                          value = NULL,
+                          min = NA,
+                          max = NA,
+                          step = NA,
+                          width = NULL
+                      )
+    ),
+    mainPanel(tabsetPanel(
+        tabPanel("Plot", dataTableOutput("contents")),
+        tabPanel("Individual", plotOutput("hist1")),
+        tabPanel("Histogram" ,plotOutput("hist2"))
+        
+        
+        
+        
+    ))
 )
 
 # Define server logic required to draw a histogram
@@ -68,12 +103,12 @@ server <- function(input, output) {
         mydata
     })
     
-
+    
     # Dynamically generate UI input when data is uploaded ----
     output$dropdown <- renderUI({
         selectInput(inputId = "measure", 
-                           label = "Select Measure", 
-                           choices = names(winedata()))
+                    label = "Select Measure", 
+                    choices = names(winedata()))
     })
     
     # Select columns to print ----
@@ -92,72 +127,87 @@ server <- function(input, output) {
         #DT::datatable
         winedata()
     })
-     output$hist1 <- renderPlot({
-         
-         tempDf <- winedata()
-         
-         tempDf$row_num <- seq.int(nrow(tempDf))
-         
-         tempDf <- tempDf[1:60,]
-         
-         which(names(tempDf)==input$measure)
-         
-         print((noquote(input$measure)))
-         
+    output$hist1 <- renderPlot({
         
-         
-         tempVar <- sym(input$measure)
-         
-         INDV <- ggplot(tempDf, aes(x = row_num, y= !!tempVar)) +
-             geom_point() + geom_line() +
-             ggtitle("XmR", "geom_point() and geom_line() required") + 
-             ylim(-4,16) + #make scales same
-             scale_x_continuous(expand =  expand_scale(mult = .15)) + #pad x-axis
-             stat_QC(method="XmR", auto.label = T) +
-             ggtitle("XmR", subtitle = "Auto Label QC lines")
-         
-         plot(INDV)
-     })  
-     output$hist2 <- renderPlot({
-         
-         
-         tempDf <- winedata()
-         
-         tempDf$row_num <- seq.int(nrow(tempDf))
-         
-         tempDf <- tempDf[1:60,]
-         
-         which(names(tempDf)==input$measure)
-         
-         print((noquote(input$measure)))
-         
-         
-         
-         tempVar <- sym(input$measure)
-         
-         UCL= mean(tempDf$fixed_acidity) + 3*sd(tempDf$fixed_acidity)
-         LCL= mean(tempDf$fixed_acidity) - 3*sd(tempDf$fixed_acidity)
-         
-         CapabilityAnaylsis <- 
-             ggplot(tempDf, aes(x = fixed_acidity)) + #init ggplot
-             geom_histogram(binwidth = .75, color="purple") + #make the histogram
-             stat_QC_Capability(
-                 LSL=4.50, USL=17.00,                    #Specify LSL and USL
-                 show.cap.summary = c("Cp", "Cpk"), #selected summary
-                 digits = 2,                        #report two digits
-                 method="XmR") +                    #Use the XmR method
-             scale_x_continuous(expand =  expand_scale(mult = c(0.15,.65)))+ #pad the X-axis
-             geom_vline(xintercept=mean(tempDf$fixed_acidity), colour="Blue") +
-             geom_text(aes(x=mean(tempDf$fixed_acidity)), y = 25.5, label="Mean", colour="Blue", angle=0, vjust = 1.2, text=element_text(size=11))+
-             geom_vline(xintercept=UCL, colour="Blue") +
-             geom_text(aes(x=UCL), y = 25.5, label="3sd", colour="blue", angle=0, vjust = 1.2, text=element_text(size=11))+
-             geom_vline(xintercept=LCL, colour="Blue") +
-             geom_text(aes(x=LCL), y = 25.5, label="-3sd", colour="blue", angle=0, vjust = 1.2, text=element_text(size=11))
-         
-         
-         CapabilityAnaylsis  #plot the graph
-         
-         
+        tempDf <- winedata()
+        
+        tempDf$row_num <- seq.int(nrow(tempDf))
+        
+        tempDf <- tempDf[1:60,]
+        
+        which(names(tempDf)==input$measure)
+        
+        print((noquote(input$measure)))
+        
+        
+        
+        tempVar <- sym(input$measure)
+        
+        INDV <- ggplot(tempDf, aes(x = row_num, y= !!tempVar)) +
+            geom_point() + geom_line() +
+            ggtitle("XmR", "geom_point() and geom_line() required") + 
+            ylim(-4,16) + #make scales same
+            scale_x_continuous(expand =  expand_scale(mult = .15)) + #pad x-axis
+            stat_QC(method="XmR", auto.label = T) +
+            ggtitle("XmR", subtitle = "Auto Label QC lines")
+        
+        plot(INDV)
+    })  
+    output$hist2 <- renderPlot({
+        
+        
+        tempDf <- winedata()
+        
+        tempDf$row_num <- seq.int(nrow(tempDf))
+        
+        tempDf <- tempDf[1:60,]
+        
+        which(names(tempDf)==input$measure)
+        
+        print((noquote(input$measure)))
+        
+        
+        tempVar <- sym(input$measure)
+        
+        
+        #UCL= mean(tempDf[,input$measure]) + 3*sd(tempDf[,input$measure])
+        #LCL= mean(tempDf$fixed_acidity) - 3*sd(tempDf$fixed_acidity)
+        # print(UCL)
+        # print(tempDf[,which(names(tempDf)==input$measure)])
+        # print(tempDf %>% dplyr::select(input$measure))
+        # mean(tempDf %>% dplyr::select(input$measure) %>% .[,1])
+        
+        ucl_var <- tempDf %>% dplyr::select(input$measure) %>% 
+            pull(input$measure)
+        
+        print( mean(ucl_var) )
+        
+        
+        
+        UCL= mean(ucl_var) + 3*sd(ucl_var)
+        LCL= mean(ucl_var) - 3*sd(ucl_var)
+        # print(UCL)
+        
+        CapabilityAnaylsis <- 
+            ggplot(tempDf, aes(x = !!tempVar)) + #init ggplot
+            geom_histogram(binwidth = .75, color="purple") + #make the histogram
+            stat_QC_Capability(
+                LSL=input$lslId, USL=input$uslId,                    #Specify LSL and USL
+                show.cap.summary = c("Cp", "Cpk"), #selected summary
+                digits = 2,                        #report two digits
+                method="XmR") +                    #Use the XmR method
+            scale_x_continuous(expand =  expand_scale(mult = c(0.15,.65))) #pad the X-axis
+        
+        
+        CapabilityAnaylsis + #plot the graph
+            geom_vline(xintercept=mean(ucl_var), colour="Blue") +
+            geom_text(aes(x=mean(ucl_var)), y = 25.5, label="Mean", colour="Blue", angle=0, vjust = 1.2, text=element_text(size=11))+
+            geom_vline(xintercept=UCL, colour="Blue") +
+            geom_text(aes(x=UCL), y = max(CapabilityAnaylsis$y), label="3sd", colour="blue", angle=0, vjust = 1.2, text=element_text(size=11))+
+            geom_vline(xintercept=LCL, colour="Blue") +
+            geom_text(aes(x=LCL), y = 25.5, label="-3sd", colour="blue", angle=0, vjust = 1.2, text=element_text(size=11))
+        
+        
     })
 }
 
